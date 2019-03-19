a2c0edb025608800ba3f2deeb059914218659c8c
Jacob <jhillebrand25@gmail.com>
2019-03-18 22:17:18 -0500

    Second commit

---
+++ De-adify.ps1
@@ -0,0 +1,14 @@
+﻿#Ask for the site to be de-adddddiffffied!!!!!!!!!!!!!
+$Site = Read-Host -Prompt 'Enter the site you''d like to view:'
+
+#Grab static HTML copy of the file
+wget $Site -OutFile $env:USERPROFILE\Downloads\out.html
+
+#Invoke iterator to use the PC's default browser to open the file
+ii $env:USERPROFILE\Downloads\out.html
+
+#Wait a few seconds for the file to be opened before deleting
+Start-Sleep -Seconds 7
+
+#Remove the file so as not to clutter the poor ol file system
+Remove-Item $env:USERPROFILE\Downloads\out.html
