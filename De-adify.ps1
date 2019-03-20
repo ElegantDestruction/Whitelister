a4b848f9b6ac240da5b89a4f86bdeea1a5279ff4
Jacob Hillebrand <35939404+ElegantDestruction@users.noreply.github.com>
2019-03-20 09:26:51 -0500

    Made dialogue box, suppressed console

--- De-adify.ps1
+++ De-adify.ps1
@@ -1,5 +1,7 @@
 ﻿#Ask for the site to be de-adddddiffffied!!!!!!!!!!!!!
-$Site = Read-Host -Prompt 'Enter the site you''d like to view:'
+[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
+$Site = [Microsoft.VisualBasic.Interaction]::InputBox("Enter a site to Whitelist", "Whitelister", "")
+
 
 #Grab static HTML copy of the file
 wget $Site -OutFile $env:USERPROFILE\Downloads\out.html
@@ -8,7 +10,7 @@ wget $Site -OutFile $env:USERPROFILE\Downloads\out.html
 ii $env:USERPROFILE\Downloads\out.html
 
 #Wait a few seconds for the file to be opened before deleting
-Start-Sleep -Seconds 7
+Start-Sleep -Seconds 5
 
 #Remove the file so as not to clutter the poor ol file system
 Remove-Item $env:USERPROFILE\Downloads\out.html
